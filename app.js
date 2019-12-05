8934acc86ca668c6f45b9a867dbc5dd1704d2fda
CherlenokYevgeniy <170103126@stu.sdu.edu.kz>
2019-12-06 01:17:29 +0600

    style(app.js): delete comments

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var session =require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
-//var validator1 = require('express-validator-builder')
 
 var routes = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -20,7 +19,6 @@ require('./config/passport');
 
 var app = express();
 
-// view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
@@ -45,17 +43,12 @@ app.use(function (req, res, next) {
 app.use('/user', userRoutes);
 app.use('/', routes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -66,8 +59,6 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -75,6 +66,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
