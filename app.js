d634ebf21086ae28e551a1a30114180c4fab2477
CherlenokYevgeniy <170103126@stu.sdu.edu.kz>
2019-12-06 00:45:51 +0600

    add latest changes

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
-//asdfasdf
+var session =require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+//var validator1 = require('express-validator-builder')
+
 var routes = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb://127.0.0.1:27017/shopping');
+require('./config/passport');
 
 var app = express();
 
@@ -22,9 +29,20 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false , saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
